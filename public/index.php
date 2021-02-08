2bc7b544155c09db90878c35c1b0a67906bd96c2
Pierre Grimaud <grimaud.pierre@gmail.com>
2021-02-08 21:37:59 +0100

    Update app

--- public/index.php
+++ public/index.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-use Apex\Api;
-use Apex\Response;
-use Apex\Validator;
+use Apex\{Api, Response, Validator};
 
 require_once __DIR__ . '/../vendor/autoload.php';
 $config = require_once __DIR__ . '/../config/parameters.php';
@@ -17,7 +15,7 @@ try {
     $validator = new Validator($_GET);
     $validator->check();
 
-    $api  = new Api($validator);
+    $api  = new Api($validator, $config['api_key']);
     $data = $api->fetchData();
 
     echo $response->returnData($data);
